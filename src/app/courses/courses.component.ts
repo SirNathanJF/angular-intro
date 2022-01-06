4c105902c9ec9fe2ff02d1265b118e1d4f1597f1
Nathan Flessner <nathanflessner1@gmail.com>
2022-01-06 14:14:03 -0700

    dependency injection and adding provider

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -11,8 +11,7 @@ export class CoursesComponent implements OnInit {
   title = "List of courses"
   courses;
 
-  constructor() {
-    let service = new CoursesService();
+  constructor(service: CoursesService) {
     this.courses = service.getCourses();
    }
 
